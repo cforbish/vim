1371686cf733ad86917c726501ff14b31ed05709
U-cforbish-hplap\cforbish <cforbish@cforbish-hplap.(none)>
2012-04-12 20:58:12 -0400

    Update python template script.

--- templates/python.py
+++ templates/python.py
@@ -1,14 +1,15 @@
 sil! exec 'r ' . expand('<sfile>')
 sil! exec '!chmod +x ' . expand('%')
-sil! normal k6dd
+sil! normal k7dd
+sil! update
 finish
 
 #!/usr/bin/python
-# vi: set ts=8 sw=3 sts=3 et:
+# vi: set ts=8 sw=4 sts=4 et:
 
 def main():
-   print "Hello World."
+    print "Hello World."
 
 if ( __name__ == "__main__"):
-   main()
+    main()
 
