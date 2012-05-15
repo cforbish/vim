fadd209e641ac0e608c3a2fc589d74b5bc07da9f
U-cforbish-hplap\cforbish <cforbish@cforbish-hplap.(none)>
2012-05-15 07:53:25 -0400

    Update several template type files to make file unix format.

--- templates/perl.pl
+++ templates/perl.pl
@@ -12,8 +12,9 @@ sil! normal d/^" <AUTO_VIM_SCRIPT_TAG 2dd
 " remove tag line 3
 sil! normal /^" <AUTO_VIM_SCRIPT_TAG 3dd
 sil! 1d
-sil! set nolz
+sil! set ff=unix
 sil! update | edit
+sil! set nolz
 finish
 " <AUTO_VIM_SCRIPT_TAG 2>
 #!/usr/bin/env perl
