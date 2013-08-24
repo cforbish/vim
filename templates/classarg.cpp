314d0cc4d25784780c622df4f8ea3ec3753cbea2
Chuck Forbish <cforbish@gmail.com>
2013-08-24 12:57:25 -0400

    Remove tags from classarg.cpp.

--- templates/classarg.cpp
+++ templates/classarg.cpp
@@ -1,10 +1,10 @@
 " <AUTO_VIM_SCRIPT_TAG 1>
 class classname
 {
-	public:
-		classname();
-		virtual ~classname();
-	private:
+    public:
+        classname();
+        virtual ~classname();
+    private:
 };
 
 // classname::classname()
